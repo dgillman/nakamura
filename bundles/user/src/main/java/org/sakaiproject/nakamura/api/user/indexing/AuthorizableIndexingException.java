d979ef038c8f8f458d3951ae7ea43dd18826ffc2
Erik Froese <erik.froese@gmail.com>
2012-06-25 11:32:27 -0400

    ACAD-1073 Reconcile custom user classes with the user api,imple split.

--- bundles/user/src/main/java/org/sakaiproject/nakamura/api/user/indexing/AuthorizableIndexingException.java
+++
@@ -1,9 +0,0 @@
-package org.sakaiproject.nakamura.api.user.indexing;
-
-/**
- * User: duffy
- * Date: Mar 16, 2012
- * Time: 9:47:23 AM
- */
-public class AuthorizableIndexingException extends Exception {
-}
