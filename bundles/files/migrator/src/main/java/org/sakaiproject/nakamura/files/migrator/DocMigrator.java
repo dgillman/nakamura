b6398dc819ab7cc3312f83e49fa37c157c250d8c
Lance Speelmon <lance@speelmon.com>
2012-08-14 16:11:22 -0700

    ACAD-1191 make logging more effective for DocMigrator in error conditions

--- bundles/files/migrator/src/main/java/org/sakaiproject/nakamura/files/migrator/DocMigrator.java
+++ bundles/files/migrator/src/main/java/org/sakaiproject/nakamura/files/migrator/DocMigrator.java
@@ -90,6 +90,7 @@ public class DocMigrator implements FileMigrationService {
     try {
       return !(content == null || isNotSakaiDoc(content) || schemaVersionIsCurrent(content) || contentHasUpToDateStructure(content));
     } catch (SakaiDocMigrationException e) {
+      LOGGER.error(e.getLocalizedMessage(), e);
       LOGGER.error("Could not determine requiresMigration with content {}", content.getPath());
       throw new RuntimeException("Could not determine requiresMigration with content " + content.getPath());
     }
@@ -141,7 +142,7 @@ public class DocMigrator implements FileMigrationService {
         try {
           adminSession.logout();
         } catch (ClientPoolException e) {
-          LOGGER.error(e.getMessage());
+          LOGGER.error(e.getLocalizedMessage(), e);
         }
       }
     }
@@ -187,14 +188,14 @@ public class DocMigrator implements FileMigrationService {
         }
       }
     } catch (Exception e) {
-      LOGGER.error(e.getMessage());
-      throw new RuntimeException("Error while migrating " + content.getPath());
+      LOGGER.error(e.getLocalizedMessage(), e);
+      throw new RuntimeException("Error while migrating " + content.getPath(), e);
     } finally {
       if (adminSession != null) {
         try {
           adminSession.logout();
         } catch (ClientPoolException e) {
-          LOGGER.error(e.getMessage());
+          LOGGER.error(e.getLocalizedMessage(), e);
         }
       }
     }
@@ -217,7 +218,7 @@ public class DocMigrator implements FileMigrationService {
       }
       return contentFromJson(migratedPage);
     } catch (JSONException e) {
-      LOGGER.error(e.getLocalizedMessage());
+      LOGGER.error(e.getLocalizedMessage(), e);
       throw new RuntimeException("failed to migrate single page: " + e.getLocalizedMessage());
     }
   }
